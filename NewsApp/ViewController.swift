30e8dbc323c93382a3e0fd4de683dbfa8fe00439
AndSorokin <Andrewsorokin13@list.ru>
2022-06-27 20:20:54 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Андрей Сорокин on 27/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
