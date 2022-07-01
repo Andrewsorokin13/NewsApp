b80f969ce30165c1f3a0bba7ed741ada35958130
AndSorokin <Andrewsorokin13@list.ru>
2022-07-01 04:41:44 +0300

    add http Status Code

---
+++ NewsApp/Model/PopularModel.swift
@@ -0,0 +1,14 @@
+//
+//  PopularModel.swift
+//  NewsApp
+//
+//  Created by Андрей Сорокин on 30/06/2022.
+//
+
+import Foundation
+
+struct PopularModel {
+    let nameNews: String
+    let imageNews: String
+    let dateNews: String
+}
